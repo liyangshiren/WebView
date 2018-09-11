dce6151977da52f3af85133a10fc210ca9964138
liyangshiren <missliyang@live.com>
2018-09-11 14:33:16 +0800

    正式第一版的WebView版本安卓8.0
    包含bar 和返回键控制

---
+++ MesWebView/MesWebView/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Webkit;
+using System;
+using Android.Views;
+
+namespace MesWebView
+{
+    [Activity(Label = "江阴振宏锻造MES系统WebView", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        WebView webview;
+        DateTime? lastBackKeyDownTime;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //soure code
+            webview = FindViewById<WebView>(Resource.Id.webView);
+            webview.SetWebViewClient(new ExtendWebViewClient());
+            webview.LoadUrl("https://www.nopcommerce.com");//就这里需要改改根据具体的实际情况
+            WebSettings webSettings = webview.Settings;
+            webSettings.JavaScriptEnabled = true;
+        }
+
+        public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && e.Action == KeyEventActions.Down)
+            {
+                if (!lastBackKeyDownTime.HasValue || DateTime.Now - lastBackKeyDownTime.Value > new TimeSpan(0, 0, 2))
+                {
+                    Toast.MakeText(this.ApplicationContext, "再按一次退出程序", ToastLength.Short).Show();
+                    lastBackKeyDownTime = DateTime.Now;
+                    webview.GoBack();
+                }
+                else
+                {
+                    Finish();
+                }
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
+        }
+
+    }
+    internal class ExtendWebViewClient : WebViewClient
+    {
+        public override bool ShouldOverrideUrlLoading(WebView view, string url)
+        {
+            view.LoadUrl(url);
+            return true;
+        }
+    }
+}
---
+++ MesWebView/MesWebView/obj/Release/MonoAndroid81/android/src/md5907de4703ef11fa2e12c2cd7ab8a76a5/MainActivity.java
@@ -0,0 +1,56 @@
+package md5907de4703ef11fa2e12c2cd7ab8a76a5;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onKeyDown:(ILandroid/view/KeyEvent;)Z:GetOnKeyDown_ILandroid_view_KeyEvent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("MesWebView.MainActivity, MesWebView", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MesWebView.MainActivity, MesWebView", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onKeyDown (int p0, android.view.KeyEvent p1)
+	{
+		return n_onKeyDown (p0, p1);
+	}
+
+	private native boolean n_onKeyDown (int p0, android.view.KeyEvent p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
